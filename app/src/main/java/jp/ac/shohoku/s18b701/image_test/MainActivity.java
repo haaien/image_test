19505185912fed44828875cebd214c8a6181291a
haien <s18b701@shohoku.ac.jp>
2019-10-03 14:30:39 +0900

    Initial commit プロジェクトの作成
    テストー！！！

---
+++ app/src/main/java/jp/ac/shohoku/s18b701/image_test/MainActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.shohoku.s18b701.image_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
